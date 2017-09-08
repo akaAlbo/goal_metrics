85265a23c1979185c2b074f0d0e088bf5032a396
ipa-flg-ma <marcel.albus@ipa.fraunhofer.de>
2017-09-08 11:46:43 +0200

    [CODE] matrix needs only 6 columns [BUGFIX] with AD class

--- goal/calculate_goal.py
+++ goal/calculate_goal.py
@@ -50,7 +50,6 @@ class CalculateGoalParamHandler:
 
 class CalculateGoal:
     def __init__(self, groundtruth, groundtruth_epsilon, topic='/base_pose_ground_truth'):
-        self.AD = AD()
         self.active = False
         self.finished = False
         self.positiontopic = topic
@@ -60,13 +59,14 @@ class CalculateGoal:
         self.targetgoal = None
         self.softgoal = None
         # create array for further use
-        self.A_listener_position = np.ones([0, 8], dtype=np.double)
-        self.A_listener_goal = np.ones([0, 8], dtype=np.double)
+        self.A_listener_position = np.ones([0, 6], dtype=np.double)
+        self.A_listener_goal = np.ones([0, 6], dtype=np.double)
 
-        rospy.Subscriber(self.positiontopic, Odometry, self.callback, queue_size=1)
+        # subscribe to topics in need
+        rospy.Subscriber(self.positiontopic, Odometry, self.callback_position, queue_size=1)
         rospy.Subscriber(self.goaltopic, PoseStamped, self.callback_goal, queue_size=1)
 
-    def callback(self, msg):
+    def callback_position(self, msg):
         if self.active:
             data_list = [float(msg.pose.pose.position.x),
                          float(msg.pose.pose.position.y),
@@ -118,42 +118,41 @@ class CalculateGoal:
         :param quaternion: list of quaternions [x, y, z, w]
         :return: euler angles
         '''
-        euler = tf.transformations.euler_from_quaternions(quaternion)
+        euler = tf.transformations.euler_from_quaternion(quaternion)
         return euler
 
     def getDistance(self):
-        delta_x = (self.A_listener_position[-1, self.AD.pX] - self.A_listener_goal[-1, self.AD.pX])
-        delta_y = (self.A_listener_position[-1, self.AD.pY] - self.A_listener_goal[-1, self.AD.pY])
+        delta_x = (self.A_listener_position[-1, AD.pX] - self.A_listener_goal[-1, AD.pX])
+        delta_y = (self.A_listener_position[-1, AD.pY] - self.A_listener_goal[-1, AD.pY])
         distance = math.sqrt(delta_x ** 2 + delta_y ** 2)
         # return delta distance in [m]
         return distance
 
     def getAngle(self):
-        quaternion_position = [self.A_listener_position[-1, self.AD.qX],
-                               self.A_listener_position[-1, self.AD.qY],
-                               self.A_listener_position[-1, self.AD.qZ],
-                               self.A_listener_position[-1, self.AD.qW]]
-        quaternion_goal = [self.A_listener_goal[-1, self.AD.qX],
-                           self.A_listener_goal[-1, self.AD.qY],
-                           self.A_listener_goal[-1, self.AD.qZ],
-                           self.A_listener_goal[-1, self.AD.qW]]
-        yaw_position = self.quaternion2euler(quaternion=quaternion_position)
-        yaw_goal = self.quaternion2euler(quaternion=quaternion_goal)
+        quaternion_position = [self.A_listener_position[-1, AD.qX],
+                               self.A_listener_position[-1, AD.qY],
+                               self.A_listener_position[-1, AD.qZ],
+                               self.A_listener_position[-1, AD.qW]]
+        quaternion_goal = [self.A_listener_goal[-1, AD.qX],
+                           self.A_listener_goal[-1, AD.qY],
+                           self.A_listener_goal[-1, AD.qZ],
+                           self.A_listener_goal[-1, AD.qW]]
+        euler_position = self.quaternion2euler(quaternion=quaternion_position)
+        euler_goal = self.quaternion2euler(quaternion=quaternion_goal)
         # return absolut value for delta yaw in [radian]
-        return math.fabs(yaw_goal - yaw_position)
+        return math.degrees(math.fabs(euler_goal[2] - euler_position[2]))
 
     def get_result(self):
         groundtruth_result = None
         details = {'topic': self.positiontopic}
 
-        # TODO: implement goal
-        # actual calculating
         if self.finished:
             if self.groundtruth != None and self.groundtruth_epsilon != None:
                 if self.getDistance() <= self.groundtruth and self.getAngle() <= self.groundtruth_epsilon:
                     data = self.getDistance()
                     groundtruth_result = True
                 else:
+                    data = -1
                     groundtruth_result = False
             return 'goal', data, groundtruth_result, self.groundtruth, self.groundtruth_epsilon, details
         else:
