a8dd9b85e1d4b2b9b5dc8fe88a85d9157e0e5cc1
ipa-flg-ma <marcel.albus@ipa.fraunhofer.de>
2017-09-19 15:11:02 +0200

    [CODE] V 2.0.0 - working

--- goal/calculate_goal.py
+++ goal/calculate_goal.py
@@ -4,6 +4,7 @@ import numpy as np
 import math
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseStamped
+from move_base_msgs.msg import MoveBaseActionGoal
 import tf
 
 
@@ -44,18 +45,25 @@ class CalculateGoalParamHandler:
                     testblock_name)
                 groundtruth = None
                 groundtruth_epsilon = None
-            metrics.append(CalculateGoal(metric["topic"], groundtruth, groundtruth_epsilon))
+            # metrics.append(CalculateGoal(metric["topic"], groundtruth, groundtruth_epsilon))
+            metrics.append(
+                CalculateGoal(metric["topic"], metric["groundtruth_angle"], metric["groundtruth_angle_epsilon"],
+                              groundtruth, groundtruth_epsilon))
         return metrics
 
 
 class CalculateGoal:
-    def __init__(self, topic, groundtruth, groundtruth_epsilon):
+    # REVIEW: copy file from atf to calculate_goal
+    # TODO: check if /move_base_simple/goal takes goal too
+    def __init__(self, topic, groundtruth_angle, groundtruth_angle_epsilon, groundtruth, groundtruth_epsilon):
         self.active = False
         self.finished = False
-        self.positiontopic = topic
-        self.goaltopic = '/move_base_simple/goal'
+        self.positiontopic = '/base_pose_ground_truth'
+        self.goaltopic = topic
         self.groundtruth = groundtruth
         self.groundtruth_epsilon = groundtruth_epsilon
+        self.groundtruth_angle = float(groundtruth_angle)
+        self.groundtruth_angle_epsilon = float(groundtruth_angle_epsilon)
         self.targetgoal = None
         self.softgoal = None
         # create array for further use
@@ -63,31 +71,43 @@ class CalculateGoal:
         self.A_listener_goal = np.ones([0, 6], dtype=np.double)
 
         # subscribe to topics in need
-        rospy.Subscriber(self.positiontopic, Odometry, self.callback_position, queue_size=1)
-        rospy.Subscriber(self.goaltopic, PoseStamped, self.callback_goal, queue_size=1)
+        rospy.Subscriber(self.positiontopic, Odometry, self.callback_position, queue_size=10)
+        # /move_base/goal has MoveBaseActionGoal msg type
+        rospy.Subscriber(self.goaltopic, MoveBaseActionGoal, self.callback_goal, queue_size=10)
 
     def callback_position(self, msg):
-        if self.active:
-            data_list = [float(msg.pose.pose.position.x),
-                         float(msg.pose.pose.position.y),
-                         float(msg.pose.pose.orientation.x),
-                         float(msg.pose.pose.orientation.y),
-                         float(msg.pose.pose.orientation.z),
-                         float(msg.pose.pose.orientation.w)]
+        # if self.active:
+        data_list = [float(msg.pose.pose.position.x),
+                     float(msg.pose.pose.position.y),
+                     float(msg.pose.pose.orientation.x),
+                     float(msg.pose.pose.orientation.y),
+                     float(msg.pose.pose.orientation.z),
+                     float(msg.pose.pose.orientation.w)]
 
-            # append data to array
-            self.A_listener_position = np.append(self.A_listener_position,
-                                                 [[data_list[0], data_list[1], data_list[2], data_list[3], data_list[4],
-                                                   data_list[5]]], axis=0)
+        # append data to array
+        self.A_listener_position = np.append(self.A_listener_position,
+                                             [[data_list[0], data_list[1], data_list[2], data_list[3], data_list[4],
+                                               data_list[5]]], axis=0)
 
     def callback_goal(self, msg):
+        # msg bei /move_base_simple/goal topic
+        # msg.pose.position.x
+        # msg.pose.position.y
+        # msg.pose.orientation.x
+        # msg.pose.orientation.y
+        # msg.pose.orientation.z
+        # msg.pose.orientation.w
         if self.active:
-            data_list = [float(msg.pose.position.x),
-                         float(msg.pose.position.y),
-                         float(msg.pose.orientation.x),
-                         float(msg.pose.orientation.y),
-                         float(msg.pose.orientation.z),
-                         float(msg.pose.orientation.w)]
+            data_list = [float(msg.goal.target_pose.pose.position.x),
+                         float(msg.goal.target_pose.pose.position.y),
+                         float(msg.goal.target_pose.pose.orientation.x),
+                         float(msg.goal.target_pose.pose.orientation.y),
+                         float(msg.goal.target_pose.pose.orientation.z),
+                         float(msg.goal.target_pose.pose.orientation.w)]
+
+            rospy.loginfo('\033[91m' + '----callback goal----' + '\033[0m')
+            rospy.loginfo('\033[1m' + 'Goal X: ' + str(data_list[0]) + '\t[m]' + '\033[0m')
+            rospy.loginfo('\033[1m' + 'Goal Y: ' + str(data_list[1]) + '\t[m]' + '\033[0m')
 
             # append data to array
             self.A_listener_goal = np.append(self.A_listener_goal,
@@ -103,6 +123,14 @@ class CalculateGoal:
         self.active = False
         self.stop_time = timestamp
         self.finished = True
+
+
+        rospy.loginfo('\033[94m' + '=' * 82 + '\033[0m')
+        result = self.get_result()
+        rospy.loginfo('\033[94m' + '=' * 82 + '\033[0m')
+        rospy.loginfo('\033[91m' + '=' * 82 + '\033[0m')
+        rospy.loginfo('\033[91m' + 'Result: ' + str(result) + '\033[0m')
+        rospy.loginfo('\033[91m' + '=' * 82 + '\033[0m')
 
     def pause(self, timestamp):
         # TODO: Implement pause time calculation
@@ -142,17 +170,63 @@ class CalculateGoal:
         # return absolut value for delta yaw in [radian]
         return math.degrees(math.fabs(euler_goal[2] - euler_position[2]))
 
+    def terminal_output(self, bool):
+        '''
+        print information to terminal
+        :param bool: True - distance < groundtruth_epsilon - output is green
+        :param bool: False - distance > groundtruth_epsilon - output is red
+        :return: --
+        '''
+        if bool:
+            rospy.loginfo('\033[92m' + 'Distance:\t\t\t' + str(self.getDistance()) + ' [m]' + '\033[0m')
+            rospy.loginfo('\033[92m' + 'Angle:\t\t\t' + str(self.getAngle()) + ' [degree]' + '\033[0m')
+            rospy.loginfo('\033[92m' + 'Groundtruth:\t\t\t' + str(self.groundtruth) + ' [m]' + '\tType: ' + str(
+                type(self.groundtruth)) + '\033[0m')
+            rospy.loginfo(
+                '\033[92m' + 'Groundtruth Epsilon:\t\t' + str(self.groundtruth_epsilon) + ' [m]' + '\tType: ' + str(
+                    type(self.groundtruth_epsilon)) + '\033[0m')
+            rospy.loginfo(
+                '\033[92m' + 'Groundtruth Angle:\t\t' + str(self.groundtruth_angle) + ' [degree]' + '\tType: ' + str(
+                    type(self.groundtruth_angle)) + '\033[0m')
+            rospy.loginfo('\033[92m' + 'Groundtruth Angle Epsilon:\t' + str(
+                self.groundtruth_angle_epsilon) + ' [degree]' + '\tType: ' + str(
+                type(self.groundtruth_angle_epsilon)) + '\033[0m')
+
+        else:
+            rospy.loginfo('\033[91m' + 'Distance:\t\t\t' + str(self.getDistance()) + ' [m]' + '\033[0m')
+            rospy.loginfo('\033[91m' + 'Angle:\t\t\t' + str(self.getAngle()) + ' [degree]' + '\033[0m')
+            rospy.loginfo('\033[91m' + 'Groundtruth:\t\t\t' + str(self.groundtruth) + ' [m]' + '\tType: ' + str(
+                type(self.groundtruth)) + '\033[0m')
+            rospy.loginfo(
+                '\033[91m' + 'Groundtruth Epsilon:\t\t' + str(self.groundtruth_epsilon) + ' [m]' + '\tType: ' + str(
+                    type(self.groundtruth_epsilon)) + '\033[0m')
+            rospy.loginfo('\033[91m' + 'Groundtruth Angle:\t\t' + str(self.groundtruth_angle) + '\tType: ' + str(
+                type(self.groundtruth_angle)) + '\033[0m')
+            rospy.loginfo('\033[91m' + 'Groundtruth Angle Epsilon:\t' + str(
+                self.groundtruth_angle_epsilon) + ' [degree]' + '\tType: ' + str(
+                type(self.groundtruth_angle_epsilon)) + '\033[0m')
+
     def get_result(self):
         groundtruth_result = None
-        details = {'topic': self.positiontopic}
-
+        details = {'topic': self.goaltopic}
         if self.finished:
+            # save file for debugging
+            f = open('/home/flg-ma/PycharmProjects/goal_metrics/debug/calculate_goal.txt', 'w')
+            f.write(str(self.A_listener_position))
+            f.write('\n' + '=' * 20 + '\n')
+            f.write(str(self.A_listener_goal))
+            f.close()
+
             if self.groundtruth != None and self.groundtruth_epsilon != None:
-                if self.getDistance() <= self.groundtruth and self.getAngle() <= self.groundtruth_epsilon:
+                # if distance <= self.groundtruth and angle <= self.groundtruth_epsilon:
+                if (self.getDistance() - self.groundtruth) <= self.groundtruth_epsilon and \
+                                (self.getAngle() - self.groundtruth_angle) <= self.groundtruth_angle_epsilon:
+                    self.terminal_output(True)
                     data = self.getDistance()
                     groundtruth_result = True
                 else:
-                    data = -1
+                    self.terminal_output(False)
+                    data = self.getDistance()
                     groundtruth_result = False
             return 'goal', data, groundtruth_result, self.groundtruth, self.groundtruth_epsilon, details
         else:
